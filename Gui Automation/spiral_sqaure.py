1716d17232671c7dd95fb55ece1465f72c29292b
Yogesh Singh Thakur <yt11111999@gmail.com>
2020-07-23 23:10:58 +0530

    Update spiral_sqaure.py

--- Gui Automation/spiral_sqaure.py
+++ Gui Automation/spiral_sqaure.py
@@ -1,10 +1,11 @@
 import pyautogui as gui
 gui.moveTo(112,346)
+#set the coordinates according to your screen, mine might not works on yours
 gui.click()
 distance = 400
-slow_one = 0
+slow_one = true
 while distance > 0:
-    if slow_one == 0:
+    if slow_one == true:
         print('right, ',end = '')
         gui.dragRel(distance,0,duration = 1)
         distance = distance - 10
@@ -15,7 +16,7 @@ while distance > 0:
         distance =distance - 10
         print('up, ',end = '')
         gui.dragRel(0,-distance,duration = 1)
-        slow_one = 1
+        slow_one = false
     else:
         print('right, ',end = '')
         gui.dragRel(distance,0,duration = 0.1)
